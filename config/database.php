e7cbb55987882912f9bc1102b38096d69f0cc0a1
Wayami <kittymashallllbremenno@gmail.com>
2023-03-11 02:03:42 +1000

    postgresql is working

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.2'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'todo_u3zk'),
+            'username' => env('DB_USERNAME', 'wayami'),
+            'password' => env('DB_PASSWORD', 'M0BSzTJdBckC4HLQ6IOzHbwdmyLPQazg'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
